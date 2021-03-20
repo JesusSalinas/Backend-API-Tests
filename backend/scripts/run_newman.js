d6ada6458a0ba9b0fe63edf96c622d28cfe6f4c9
Jesus <jesus@MacBook-Pro.local>
2021-03-19 19:47:12 -0600

    newman with cli

---
+++ backend/scripts/run_newman.js
@@ -0,0 +1,32 @@
+const newman = require('newman');
+
+newman.run({
+    collection: './pathToFile/collection.json', // Collection URL from a public link or the Postman API can also be used
+    reporters: ['htmlextra'],
+    iterationCount: 1,
+    reporter: {
+        htmlextra: {
+            // export: './report.html',
+            // template: './template.hbs'
+            // logs: true,
+            // showOnlyFails: true,
+            // noSyntaxHighlighting: true,
+            // testPaging: true,
+            // browserTitle: "My Newman report",
+            // title: "My Newman Report",
+            // titleSize: 4,
+            // omitHeaders: true,
+            // skipHeaders: "Authorization",
+            // hideRequestBody: ["Login"],
+            // hideResponseBody: ["Auth Request"],
+            // showEnvironmentData: true,
+            // skipEnvironmentVars: ["API_KEY"],
+            // showGlobalData: true,
+            // skipGlobalVars: ["API_TOKEN"],
+            // skipSensitiveData: true,
+            // showMarkdownLinks: true,
+            // showFolderDescription: true,
+            // timezone: "Australia/Sydney"
+        }
+    }
+});
