da758536b283b2f6b8d39637cc7de31431f57f12
Jesus <jesus@MacBook-Pro.local>
2021-03-19 21:49:23 -0600

    newman with script and dotenv

--- backend/scripts/run_newman.js
+++ backend/scripts/run_newman.js
@@ -1,20 +1,42 @@
 const newman = require('newman');
+const dotenv = require('dotenv');
+dotenv.config();
 
 newman.run({
-    collection: './pathToFile/collection.json', // Collection URL from a public link or the Postman API can also be used
+    collection: './backend/collections/tasks.json', 
     reporters: ['htmlextra'],
     iterationCount: 1,
+    environment: {
+        "id": "bd65ffff-6443-41b1-832f-bd33940330ec",
+        "name": "integration",
+        "values": [
+            {
+                "key": "token",
+                "value": `${process.env.API_KEY}`,
+                "enabled": true
+            },
+            {
+                "key": "api",
+                "value": `${process.env.URL}`,
+                "enabled": true
+            },
+            {
+                "key": "projectId",
+                "value": "2260972332",
+                "enabled": true
+            }
+        ],
+        "_postman_variable_scope": "environment",
+        "_postman_exported_at": "2021-03-20T00:56:11.533Z",
+        "_postman_exported_using": "Postman/8.0.7"
+    },
     reporter: {
         htmlextra: {
-            // export: './report.html',
-            // template: './template.hbs'
-            // logs: true,
-            // showOnlyFails: true,
-            // noSyntaxHighlighting: true,
-            // testPaging: true,
-            // browserTitle: "My Newman report",
-            // title: "My Newman Report",
-            // titleSize: 4,
+            export: './backend/reports/report.html',
+            logs: true,
+            browserTitle: "NEWMAN REPORT",
+            title: "WORKSHOP WIZELINE",
+            titleSize: 4
             // omitHeaders: true,
             // skipHeaders: "Authorization",
             // hideRequestBody: ["Login"],
@@ -28,5 +50,14 @@ newman.run({
             // showFolderDescription: true,
             // timezone: "Australia/Sydney"
         }
+    }
+}).on('start', function (err, args) { // on start of run, log to console
+    console.log('######  Running a collection...   #######', '\n');
+}).on('done', function (err, summary) {
+    if (err || summary.error) {
+        console.error('   ','######### Collection run encountered an error. #######');
+    }
+    else {
+        console.log('  ','#### Collection run completed. ');
     }
 });
